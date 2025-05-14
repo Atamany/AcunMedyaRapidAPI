16613f4ff9f0fb53a41e00aaf2574ca5131a090d
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-05-14 21:21:59 +0300

    14.05.2025 - Adım 2

---
+++ AcunMedyaRapidAPI/Controllers/ExchangeController.cs
@@ -0,0 +1,31 @@
+﻿using AcunMedyaRapidAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AcunMedyaRapidAPI.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&to_symbol=TRY&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "f3199434e5msh58ee98c4e68e750p14cb70jsn56545f8ecbb0" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(value);
+            }
+        }
+    }
+}
