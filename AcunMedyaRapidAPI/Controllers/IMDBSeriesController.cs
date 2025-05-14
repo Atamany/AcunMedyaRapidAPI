5533b1929216612513e8a290ec4bbe885376445b
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-05-14 20:30:02 +0300

    Proje dosyası ekle.

---
+++ AcunMedyaRapidAPI/Controllers/IMDBSeriesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcunMedyaRapidAPI.Controllers
+{
+    public class IMDBSeriesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "f3199434e5msh58ee98c4e68e750p14cb70jsn56545f8ecbb0" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
+            return View();
+        }
+    }
+}
---
+++ AcunMedyaRapidAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AcunMedyaRapidAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
